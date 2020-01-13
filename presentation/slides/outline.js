f44510270adf313f2e2a59416cbc3b944cc7fb57
mbeche <mb.johnsonbece@gmail.com>
2020-01-12 21:13:34 -0600

    Add repo to last slide

--- presentation/slides/outline.js
+++ presentation/slides/outline.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import { Slide, Heading, List, ListItem } from 'spectacle';
+import styled from 'styled-components';
+
+export const RepoText = styled.a`
+  color: #fded07;
+  text-decoration: none;
+  font-size: 42px;
+  line-height: 80px;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
 
 export const Outline = () => (
   <Slide>
     <Heading>Dojo Gameplan</Heading>
+    <RepoText href="github.com/micleners/gatsby-contentful">
+      github.com/micleners/gatsby-contentful
+    </RepoText>
     <List style={{ color: '#bbbbbb' }}>
       <ListItem>Set up Gatsby for Contentful</ListItem>
       <ListItem>Create Model and Data in Contentful</ListItem>
